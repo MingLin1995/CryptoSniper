6530bedabde8f062bf9c28170ebf2d1f955f097a
Ming <ben014335@gmail.com>
2023-11-04 10:48:12 +0800

    feat：前端畫面初版  因應新需求做調整： 1.前端畫面初版 調整項目： modified:   public/css/index.css new file:   public/js/Bootstrap.js modified:   public/js/User.js modified:   views/index.html

---
+++ public/js/Bootstrap.js
@@ -0,0 +1,8 @@
+// public/js/Bootstrap.js
+
+// Example function to toggle night mode
+document
+  .getElementById("toggleThemeBtn")
+  .addEventListener("click", function () {
+    document.body.classList.toggle("night-mode");
+  });
--- public/js/User.js
+++ public/js/User.js
@@ -1,23 +1,41 @@
 // public/js/User.js
 
-function isUserLoggedIn() {
-  // 檢查 localStorage 是否有 token
-  return localStorage.getItem("token") !== null;
+document.addEventListener("DOMContentLoaded", () => {
+  checkLoginStatus();
+});
+
+function checkLoginStatus() {
+  const token = localStorage.getItem("token");
+  const loginBtn = document.getElementById("loginBtn");
+  const logoutBtn = document.getElementById("logoutBtn");
+  const trackingForm = document.getElementById("trackingForm");
+  const hiddenBeforeLoginElements = document.querySelectorAll(
+    ".hidden-before-login"
+  );
+
+  if (token) {
+    loginBtn.style.display = "none";
+    logoutBtn.style.display = "inline-block";
+    trackingForm.style.display = "block";
+    getTelegramId();
+
+    // 顯示所有需要在登入後顯示的元素
+    hiddenBeforeLoginElements.forEach((el) => {
+      el.style.display = "block";
+    });
+  } else {
+    loginBtn.style.display = "inline-block";
+    logoutBtn.style.display = "none";
+    document.getElementById("telegramId").value = "";
+    trackingForm.style.display = "none";
+    // 隱藏所有需要在登入前隱藏的元素
+    hiddenBeforeLoginElements.forEach((el) => {
+      el.style.display = "none";
+    });
+  }
 }
 
-// 在頁面加載時檢查用戶的登錄狀態
-window.onload = function () {
-  if (isUserLoggedIn()) {
-    // 如果用戶已登入，則獲取和顯示 Telegram ID
-    getTelegramId();
-  } else {
-    // 如果用戶未登入，則不顯示 Telegram ID 相關信息
-    document.getElementById("telegramId").value = "";
-  }
-};
-
 async function getTelegramId() {
-  // 從伺服器或localStorage獲取用戶的Telegram ID，並設置到輸入框中
   const savedTelegramId = localStorage.getItem("telegramId");
   if (savedTelegramId) {
     document.getElementById("telegramId").value = savedTelegramId;
@@ -50,7 +68,6 @@ async function register() {
   }
 }
 
-// 登入
 async function login() {
   const email = document.getElementById("loginEmail").value;
   const password = document.getElementById("loginPassword").value;
@@ -66,14 +83,15 @@ async function login() {
 
     if (response.ok) {
       alert("登入成功！" + data.message);
-      localStorage.setItem("token", data.token); // 將 token 儲存到瀏覽器的 localStorage
+      localStorage.setItem("token", data.token);
 
-      // 假設伺服器返回的數據中包含了 telegramId
       if (data.telegramId) {
-        localStorage.setItem("telegramId", data.telegramId); // 將 telegramId 儲存到 localStorage
+        localStorage.setItem("telegramId", data.telegramId);
+        document.getElementById("telegramId").value = data.telegramId;
       }
 
-      location.reload(); // 重新加載頁面
+      $("#loginModal").modal("hide");
+      checkLoginStatus();
     } else {
       alert("登入失敗：" + data.error);
     }
@@ -82,8 +100,6 @@ async function login() {
     alert("伺服器錯誤");
   }
 }
-
-// 登出
 
 async function logout() {
   try {
@@ -97,9 +113,9 @@ async function logout() {
 
     if (response.ok) {
       localStorage.removeItem("token");
-      localStorage.removeItem("telegramId"); // 登出時也移除 telegramId
+      localStorage.removeItem("telegramId");
       alert("登出成功！");
-      location.reload();
+      checkLoginStatus();
     } else {
       const data = await response.json();
       alert("登出失敗：" + data.error);
