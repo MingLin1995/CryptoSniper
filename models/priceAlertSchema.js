dbc2880ce0d6debf83eaa1f44a621bf288041fd2
Ming <ben014335@gmail.com>
2023-11-27 16:22:06 +0800

    README修改

--- models/priceAlertSchema.js
+++ models/priceAlertSchema.js
@@ -2,30 +2,25 @@
 
 const mongoose = require("mongoose");
 
-const priceAlertSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    symbol: {
-      type: String,
-      required: true,
-    },
-    targetPrice: {
-      type: Number,
-      required: true,
-    },
-    notificationMethod: {
-      type: String,
-      required: true,
-      enum: ["Telegram", "Web", "Line"],
-    },
+const priceAlertSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  symbol: {
+    type: String,
+    required: true,
+  },
+  targetPrice: {
+    type: Number,
+    required: true,
+  },
+  notificationMethod: {
+    type: String,
+    required: true,
+    enum: ["Telegram", "Web", "Line"],
+  },
+});
 
 module.exports = mongoose.model("PriceAlert", priceAlertSchema);
